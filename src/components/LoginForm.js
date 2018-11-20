8d2806c87748aa5cbd1a2b03c847ec3446d7d8c5
DonovanDwyer <dwyer.donovan@gmail.com>
2018-11-20 13:59:00 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,43 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  formOnSubmit = (event) => {
+    console.log(this.state.username)
+    event.preventDefault();
+    if(this.state.username !== "" && this.state.password !== ""){
+      this.props.onSubmit(this.state)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.formOnSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.changeHandler}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.changeHandler} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" >Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" name="message" value={this.state.message} onChange={this.changeHandler}/>
+        <small>{this.props.maxChars - this.state.message.length} characters left</small>
       </div>
     );
   }
